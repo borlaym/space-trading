0603b4ac3ad3bad38747b076fd1f76026a8ea462
Marton Borlay <borlaym@gmail.com>
2019-02-20 20:46:13 +0100

    Update game engine

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as THREE from 'three';
+import './index.css'
+import { GameScene, GameCamera, GameWorld, GameObject, Collision, InputController, Rendering, Outline, Selectable } from 'game-engine'
+import { Vector3 } from 'three';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const gameScene = new GameScene()
+GameCamera.position.y = 4
+GameCamera.position.z = 7
+GameCamera.position.x = 2.5
+GameCamera.camera.lookAt(new Vector3(2.5, 0, 2.5))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let lastTick: number = Date.now()
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function update() {
+	const now = Date.now()
+	const dt = now - lastTick
+	lastTick = now
+
+	// Update colliders
+	GameObject.getComponentsOfType(Collision).forEach(c => c.update(dt))
+
+	// Update InputController
+	InputController.update()
+
+	// Update camera
+	GameCamera.update(dt)
+
+	// Update gameObjects
+
+	// Update meshes
+	GameObject.getComponentsOfType(Rendering).forEach(c => c.update(dt))
+
+	// Update outlines
+	GameObject.getComponentsOfType(Outline).forEach(c => c.update(dt))
+
+	// Update selects
+	GameObject.getComponentsOfType(Selectable).forEach(c => c.update())
+	Selectable.checkClear()
+
+	// Reset InputController
+	InputController.reset()
+
+	// Render
+	renderer.render(gameScene.scene, GameCamera.camera);
+	requestAnimationFrame(update)
+}
+
+update()
